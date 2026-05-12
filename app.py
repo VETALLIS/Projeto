d91f0540068cba542ded7a5b44d122e6a6c1640c
Rafael Piva Bazani <vetalisge@gmail.com>
2026-05-12 09:52:09 -0300

    atualizando erro

--- app.py
+++ app.py
@@ -570,7 +570,7 @@ def produto_entrada():
 def produto_saida():
     return render_template("informacao_produto.html")
 
-@app.route("lista_compra/")
+@app.route("/lista_compra")
 def lista_compra():
     return render_template("lista_compra.html")
 
