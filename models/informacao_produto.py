c7805ac3da744a7d8a62970db83e8b0b7f525953
Vitória Baião <vetalisge@gmail.com>
2026-05-12 11:38:30 -0300

    update informacoes

--- models/informacao_produto.py
+++ models/informacao_produto.py
@@ -15,6 +15,18 @@ class produto(Crud_base):
         self.produto_categoria = produto_categoria
         self.usuario_usuario_id = usuario_usuario_id
 
+    @classmethod
+    def low_stock(cls):
+        conexao = Database.connect()
+        cursor = conexao.cursor(dictionary=True)
+        try:
+            sql = "SELECT * FROM produto WHERE id=%s"
+            cursor.execute(sql)
+            return cursor.fetchall()
+        finally:
+            cursor.close()
+            conexao.close()
+
     
     def deletar_produto(self, id):
         produto = self.buscar_por_id(id)
