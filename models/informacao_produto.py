be9bfa1c19001dc5c48dfbc833b0e9bd73402403
Vitória Baião <vetalisge@gmail.com>
2026-05-12 11:23:54 -0300

    update gitignore

---
+++ models/informacao_produto.py
@@ -0,0 +1,43 @@
+from core.crud_base import Crud_base
+from core.manipular import Manipular
+
+class produto(Crud_base):
+
+    # Define a tabela e os campos do banco
+    tabela = "produto"
+    fields = ["produto_id","produto_nome", "produto_descricao", "produto_categoria", "usuario_usuario_id"]
+
+    # Define os atributos 
+    def __init__(self, produto_id, produto_nome, produto_descricao, produto_categoria, usuario_usuario_id):
+        self.produto_id = produto_id
+        self.produto_nome = produto_nome
+        self.produto_descricao = produto_descricao
+        self.produto_categoria = produto_categoria
+        self.usuario_usuario_id = usuario_usuario_id
+
+    
+    def deletar_produto(self, id):
+        produto = self.buscar_por_id(id)
+
+        if not produto:
+            raise ValueError("Produto não encontrado")
+
+        self.deletar()
+        return "Produto deletado com sucesso!"
+
+    def atualizar_produto(self, id):
+        produto = self.buscar_por_id(id)
+
+        if not produto:
+            raise ValueError("Produto não encontrado")
+
+        self.atualizar()
+        return "Produto atualizado com sucesso!"
+
+    def buscar_produto(self):
+        produto  = self.buscar_por_id(id)
+
+        if not produto:
+            raise ValueError("Produto não encontrado!")
+
+        return produto(**produto)
