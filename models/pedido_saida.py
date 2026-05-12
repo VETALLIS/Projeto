6e51a626d7056851058b5efcc14bf61f4c020fde
Enzo-Senai <vetalisge@gmail.com>
2026-05-12 13:40:21 -0300

    create_pedido_saida_crud

---
+++ models/pedido_saida.py
@@ -0,0 +1,56 @@
+from core.crud_base import Crud_base
+from core.manipular import Manipular 
+
+class Fornecedor(Crud_base):
+    tabela = "pedido_saida"
+    fields = ["pedido_saida_id", "pedido_saida_nome", "pedido_saida_data", "pedido_saida_status", "animal_animal_id"]
+
+    def __init__(self, nome, data, status = "PENDENTE", animal ):
+        self.pedido_saida_nome = nome
+        self.pedido_saida_data = data or datetime.now()
+        self.pedido_saida_status = status
+        self.animal_animal_id = animal
+
+    def validar_fornecedor(self):
+        erros = [
+            Manipular.validar_vazio(self.pedido_saida_nome, "nome"),
+            Manipular.validar_vazio(self.pedido_saida_data, "data"),
+            Manipular.validar_data(self.pedido_saida_data, "data"),
+            Manipular.validar_vazio(self.animal_animal_id, "animal"),     
+        ]          
+    
+        return [ erro for erro in erros if erro]
+
+    def gravar_pedido_saida(self):
+        pedido_saida = self.gravar()
+
+        if not pedido_saida:
+            raise ValueError("Erro ao criar pedido!")
+
+        return "Pedido criado com sucesso"
+
+    def deletar_pedido_saida(self, id):
+        pedido_saida = self.buscar_por_id(id)
+
+        if not pedido_saida:
+            raise ValueError("Pedido não encontrado")
+
+        self.deletar()
+        return "Pedido deletado com sucesso!"
+
+    def atualizar_pedido_saida(self, id):
+        pedido_saida = self.buscar_por_id(id)
+
+        if not pedido_saida:
+            raise ValueError("Pedido não encontrado")
+
+        self.atualizar()
+        return "Pedido atualizado com sucesso!"
+
+    def buscar_pedido_saida(self):
+        pedido_saida  = self.buscar_por_id(id)
+
+        if not pedido_saida:
+            raise ValueError("Pedido não encontrado!")
+
+        return Pedido_saida(**pedido_saida)
