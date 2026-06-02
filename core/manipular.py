78d8725537d71cd9a2cc382062e9ac2bce6d6d71
Enzo-Senai <vetalisge@gmail.com>
2026-06-02 14:14:54 -0300

    update_pedido_entrada

--- core/manipular.py
+++ core/manipular.py
@@ -89,7 +89,6 @@ class Manipular:
         meses = ['01', '02', '03', '04', '05', '06',
             '07', '08', '09', '10', '11', '12',]
         if field_name == "data":
-            print("data", dados["data"])
             if len(dados) == 10:
                 for data in dados:
                     if not(data.isdigit() or data == "/"):
--- models/pedido_entrada.py
+++ models/pedido_entrada.py
@@ -7,18 +7,18 @@ class Pedido_entrada(Crud_base):
     pk = "pedido_entrada_id"
     fields = ["pedido_entrada_nome", "pedido_entrada_data", "pedido_entrada_status", "fornecedor_fornecedor_id"]
 
-    def __init__(self, pedido_entrada_nome, pedido_entrada_data, pedido_entrada_status):
+    def __init__(self, pedido_entrada_nome, pedido_entrada_data, pedido_entrada_status, fornecedor_fornecedor_id=None):
         self.pedido_entrada_nome = pedido_entrada_nome
         self.pedido_entrada_data = pedido_entrada_data
         self.pedido_entrada_status = pedido_entrada_status
-        self.fornecedor_fornecedor_id = 1
+        self.fornecedor_fornecedor_id = fornecedor_fornecedor_id
 
     def validar_pedido_entrada (self):
         erros = [
             Manipular.validar_vazio (self.pedido_entrada_nome, "pedido_entrada_nome"),
             Manipular.validar_vazio (self.pedido_entrada_data, "pedido_entrada_data"),
             Manipular.validar_vazio (self.pedido_entrada_status, "pedido_entrada_status"),
-            Manipular.validar_data (self.pedido_entrada_data, "pedido_entrada_data")
+            Manipular.validar_data(self.pedido_entrada_data, "data")
         ]
         return [ erro for erro in erros if erro]
     
