f7f3454b0d76ce2f71498e3735172403313f0217
Rafael Piva Bazani <vetalisge@gmail.com>
2026-06-17 17:09:59 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "root",
+    "password": "123456",
     "database": "vetallis_db_2_2",
-    #"auth_plugin": "mysql_native_password"
+    "auth_plugin": "mysql_native_password"
 
 }
