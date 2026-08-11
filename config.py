90f47bec73e424af4ccc8fdbe988e1ea8ff37f97
Rafael Piva Bazani <rafael.bazani@aluno.senai.br>
2026-08-11 13:55:54 -0300

    update confg.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "root",
+    "password": "123456",
     "database": "vetallis_db_2_2",
     "auth_plugin": "mysql_native_password"
 
