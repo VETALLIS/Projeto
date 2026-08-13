be8469d6b51db878fd5e8d609e4b53b1cebb2f7b
maria-cavallaro <maria.cavallaro@aluno.senai.br>
2026-08-13 10:40:44 -0300

    modal

--- criar_admin.py
+++ criar_admin.py
@@ -1,13 +1,21 @@
 from models.usuario import Usuario
 
+# Caminho da imagem padrão
+caminho_imagem = "static/imagens/medicamento.jpg"
 
+# Lê a imagem como binário (blob)
+with open(caminho_imagem, "rb") as arquivo:
+    imagem_blob = arquivo.read()
 
 dados = {
     "usuario_nome": "Administrador",
     "usuario_email": "admin@sistema.com",
     "usuario_senha": "123456",
     "usuario_cpf": "12345678901",
-    "usuario_cargo": "adm"
+    "usuario_cargo": "adm",
+    "usuario_imagem": "medicamento.jpg",   # nome do arquivo
+    "imagem_tipo": "image/jpeg",           # tipo mime
+    "imagem_blob": imagem_blob             # conteúdo binário da imagem
 }
 
 Usuario.inserir_usuario_adm(dados)
