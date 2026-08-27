3ecb9cce2bd7aa669815d1a336c09491d676a838
VETALLIS <vetalisge@gmail.com>
2026-08-27 16:00:46 -0300

    Update api.js

--- Vetallis_mobile/vetallis_mobile/src/services/api.js
+++ Vetallis_mobile/vetallis_mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.135.60.37/api', // Substitua pelo seu IP Local
+  baseURL: 'http://10.135.60.38/api', // Substitua pelo seu IP Local
 });
 
 export default api;
