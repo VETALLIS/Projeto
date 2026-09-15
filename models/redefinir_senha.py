36eed3c832134e4c785431f3033ebc3281216112
Rafael Piva Bazani <rafael.bazani@aluno.senai.br>
2026-09-15 11:23:48 -0300

    update

--- models/redefinir_senha.py
+++ models/redefinir_senha.py
@@ -1,8 +1,13 @@
 import random
 import smtplib
 from core.crud_base import Crud_base
+from core.conectar import Database
 
 class Redefinir(Crud_base):
+
+    tabela = "recuperar"
+    fields = ["recuperar_codigo"]
+    pk = "recuperar_id"
 
     def gerar_codigo(self):
         numeros = []
@@ -12,8 +17,8 @@ class Redefinir(Crud_base):
 
         return numeros
 
-    def enviar_email(self, email):
-        codigo = self.gerar_codigo()
+    def enviar_email(self, email, codigo):
+
         try:
             servidor_email = smtplib.SMTP('smtp.gmail.com', 587)
             servidor_email.starttls()
@@ -24,6 +29,7 @@ class Redefinir(Crud_base):
             conteudo = f'Ola, este e um email de teste. {codigo}'
 
             servidor_email.sendmail(remetente, destinatario, conteudo)
+            return True
         except Exception as e:
             print(f"Erro ao enviar email: {e}")
         finally:
@@ -36,3 +42,37 @@ class Redefinir(Crud_base):
             return False
 
         return buscar
+
+    def gravar_codigo(self, numeros):
+        self.recuperar_codigo = "".join(str(n) for n in numeros)
+        gravar = self.gravar()
+
+        if not gravar:
+            return "Erro ao gravar código de recuperação"
+
+        return gravar
+
+    
+    @classmethod
+    def buscar_codigo(cls, numero):
+        conexao = Database.connect()
+        cursor = conexao.cursor(dictionary=True)
+
+        try:
+            sql = f"SELECT * FROM {cls.tabela} where recuperar_codigo = %s"
+            cursor.execute(sql, (numero,))
+            return cursor.fetchone()
+        finally:
+            cursor.close()
+            conexao.close()
+
+    def verificar_codigo(self, numeros):
+        buscar = self.buscar_codigo(numeros)
+
+        if not buscar:
+            return False
+
+        if not buscar["recuperar_codigo"] == numeros:
+            return None
+
+        return "Codigos validos"     
